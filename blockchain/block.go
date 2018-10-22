27d57a81690fec049455f03bb62150c58400a893
BytePower <bytepower@icloud.com>
2018-10-22 11:13:15 +0800

    add db

--- blockchain/block.go
+++ blockchain/block.go
@@ -3,6 +3,8 @@ package blockchain
 import (
 	"bytes"
 	"crypto/sha256"
+	"encoding/gob"
+	"log"
 	"strconv"
 	"time"
 )
@@ -22,6 +24,27 @@ func (b *Block) SetHash() {
 	b.Hash = hash[:]
 }
 
+func (b *Block) Serialize() []byte {
+	var result bytes.Buffer
+	encoder := gob.NewEncoder(&result)
+
+	err := encoder.Encode(b)
+	if err != nil {
+		log.Panic(err)
+	}
+	return result.Bytes()
+}
+
+func DeserializeBlock(d []byte) *Block {
+	var block Block
+	decoder := gob.NewDecoder(bytes.NewReader(d))
+	err := decoder.Decode(&block)
+	if err != nil {
+		log.Panic(err)
+	}
+	return &block
+}
+
 func NewBlock(data string, prevBlockHash []byte) *Block {
 	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}, 0}
 	//block.SetHash()
@@ -30,7 +53,7 @@ func NewBlock(data string, prevBlockHash []byte) *Block {
 
 	block.Hash = hash[:]
 	block.Nonce = nonce
-	
+
 	return block
 }
 
--- main.go
+++ main.go
@@ -1,20 +1,11 @@
 package main
 
-import (
-	"fmt"
-	"github.com/bytepower/blockchain/blockchain"
-)
+import "github.com/bytepower/blockchain/blockchain"
 
 func main() {
-	bc := blockchain.NewBlockchain()
-	bc.AddBlock("Send 1 BTC to Ivan")
-	bc.AddBlock("Send 2 more BTC to Ivan")
+	nbc := blockchain.NewBlockchain()
+	defer nbc.DB.Close()
 
-	for _, block := range bc.GetBlocks() {
-		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
-		fmt.Printf("Data: %s\n", block.Data)
-		fmt.Printf("Hash: %x\n", block.Hash)
-		fmt.Println()
-	}
-
+	cli := blockchain.CLI{BC: nbc}
+	cli.Run()
 }
